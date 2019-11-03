c3dfee770a3ea7b4fa15595e4d81fc42ff8220ae
DESKTOP-BDTU4QQ\marce <marcello@mono-grid.com>
2019-11-03 20:55:50 +0100

    create react app todolist-redux e map todos in app.js

---
+++ todolist-redux/todolist-redux/src/App.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state= { 
+      todos: [
+        'Fare la spesa',
+        'Fare i compiti',
+        'Chiamare la mamma'
+
+      ]
+    }
+  }
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>
+          TODO LIST
+          </h1>
+        </header>
+        <ul>
+          {
+            this.state.todos.map((todo, i)=><li key={i}>{todo}</li>)
+          }
+        </ul>
+    </div>
+    );
+  }
+}
+
+export default App;
